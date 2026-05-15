512257c2158908bff81f78b548fa4144f989baf2
rowan <crewe1746@naver.com>
2026-05-15 15:49:18 +0900

    test(stores-eth): resolve stores package from source in jest

--- packages/stores-eth/jest.config.js
+++ packages/stores-eth/jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.check.json" }],
   },
+  moduleNameMapper: {
+    "^@keplr-wallet/stores$": "<rootDir>/../stores/src",
+  },
   testMatch: ["**/src/**/?(*.)+(spec|test).[jt]s?(x)"],
 };
