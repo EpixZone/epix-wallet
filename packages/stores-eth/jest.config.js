1ae7f186a145c2a4017c92588c9721122f473aac
rowan <crewe1746@naver.com>
2026-05-15 16:11:49 +0900

    test(stores-eth): consolidate local package jest mappings

--- packages/stores-eth/jest.config.js
+++ packages/stores-eth/jest.config.js
@@ -5,12 +5,8 @@ module.exports = {
     "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.check.json" }],
   },
   moduleNameMapper: {
-    "^@keplr-wallet/common$": "<rootDir>/../common/src",
-    "^@keplr-wallet/crypto$": "<rootDir>/../crypto/src",
-    "^@keplr-wallet/simple-fetch$": "<rootDir>/../simple-fetch/src",
-    "^@keplr-wallet/stores$": "<rootDir>/../stores/src",
-    "^@keplr-wallet/types$": "<rootDir>/../types/src",
-    "^@keplr-wallet/unit$": "<rootDir>/../unit/src",
+    "^@keplr-wallet/(common|crypto|mobx-utils|simple-fetch|stores|types|unit)$":
+      "<rootDir>/../$1/src",
   },
   testMatch: ["**/src/**/?(*.)+(spec|test).[jt]s?(x)"],
 };
