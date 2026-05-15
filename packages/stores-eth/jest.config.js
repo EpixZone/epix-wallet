707f1c85a1d107c3453db248654ce2fde312cf2b
rowan <crewe1746@naver.com>
2026-05-15 15:55:29 +0900

    test(stores-eth): map local packages for clean jest runs

--- packages/stores-eth/jest.config.js
+++ packages/stores-eth/jest.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.check.json" }],
   },
   moduleNameMapper: {
+    "^@keplr-wallet/common$": "<rootDir>/../common/src",
+    "^@keplr-wallet/simple-fetch$": "<rootDir>/../simple-fetch/src",
     "^@keplr-wallet/stores$": "<rootDir>/../stores/src",
+    "^@keplr-wallet/types$": "<rootDir>/../types/src",
+    "^@keplr-wallet/unit$": "<rootDir>/../unit/src",
   },
   testMatch: ["**/src/**/?(*.)+(spec|test).[jt]s?(x)"],
 };
